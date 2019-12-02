5116d2c0479d94f9252e23dac4560f762af850e3
Jan Schmidt <me@jnschmdt.de>
2019-12-02 14:35:10 +0100

    updated torpedo

--- torpedo.m
+++ torpedo.m
@@ -11,12 +11,12 @@ time = 0; % [s]
 
 % -- Torpedo Parameter -- %
 K_p = 1; % [-] Verstärkung
-T_t = 10; % [s] Zeitkonstante Torpedo
+T_t = 15; % [s] Zeitkonstante Torpedo
 v_t = 50 * kn2mjs; % [m/s] Geschwindigkeit Torpedo (Knoten -> m/s)
 y_t__start = 4000; % [m] Startentfernung Torpedo <-> Zerstörer
 
 % -- Zerstörer Parameter -- %
-v_z = 30 * kn2mjs; % [m/s] Geschwindigkeit Zerstörer (Knoten -> m/s)
+v_z = 40 * kn2mjs; % [m/s] Geschwindigkeit Zerstörer (Knoten -> m/s)
 phi_z_zz = deg2rad(20); % [rad] Zick Zack Winkel der Zerstörers
 
 %% --- Vektoren --- %%
@@ -87,46 +87,66 @@ G_PID_D = [(T_DV * T_J) T_J 0];
 G_PID = tf(G_PID_N, G_PID_D);
 
 % Gesamtsystem
-CLOOP_T = feedback(series(G_PID, series(G_RM, G_PHI)), G_MESS);
+CLOOP_T = feedback(series(series(G_PID, G_RM), G_PHI), G_MESS);
 
+step(CLOOP_T, 40)
 %% --- Zustandsraum --- %%
-[CLOOP_T__N, CLOOP_T__D] = tfdata(CLOOP_T);
-[SS_A_CNT, SS_B_CNT, SS_C_CNT, SS_D_CNT] = tf2ss(cell2mat(CLOOP_T__N), cell2mat(CLOOP_T__D));
 
+[CLOOP_T__N, CLOOP_T__D] = tfdata(CLOOP_T, 'v');
+
+% Regelungsnormalform
+[SS_A_CNT, SS_B_CNT, SS_C_CNT, SS_D_CNT] = tf2ss(CLOOP_T__N, CLOOP_T__D);
+
+% Beobachternormalform
+
+SS_A_OBS = SS_A_CNT';
+SS_B_OBS = SS_C_CNT';
+SS_C_OBS = SS_B_CNT';
+SS_D_OBS = SS_D_CNT';
+
+SS_A = SS_A_CNT;
+SS_B = SS_B_CNT;
+SS_C = SS_C_CNT;
+SS_D = SS_D_CNT;
 %% --- Berechnung --- %%
+
+%sisotool(G_RM * G_PHI, G_PID, G_MESS)
 
 while( (sqrt((x_z(k) - x_t(k))^2 + (y_t(k) - y_z(k))^2)>=5) && ((k * dt)<t_max))
     
     % Berechnung Winkel
     phi_soll(k) = atan2((x_z(k) - x_t(k)), (y_t(k) - y_z(k)));
 
-    x1(k + 1) = x1(k) + dt * (SS_A_CNT(1,1) * x1(k)+(SS_A_CNT(1,2)) * x2(k) + (SS_A_CNT(1,3)) * x3(k) + (SS_A_CNT(1,4)) * x4(k)+(SS_A_CNT(1,5)) * x5(k) + (SS_A_CNT(1,6)) * x6(k) + SS_B_CNT(1) * phi_soll(k));
-    x2(k + 1) = x2(k) + dt * (SS_A_CNT(2,1) * x1(k)+(SS_A_CNT(2,2)) * x2(k) + (SS_A_CNT(2,3)) * x3(k) + (SS_A_CNT(2,4)) * x4(k)+(SS_A_CNT(2,5)) * x5(k) + (SS_A_CNT(2,6)) * x6(k) + SS_B_CNT(2) * phi_soll(k));
-    x3(k + 1) = x3(k) + dt * (SS_A_CNT(3,1) * x1(k)+(SS_A_CNT(3,2)) * x2(k) + (SS_A_CNT(3,3)) * x3(k) + (SS_A_CNT(3,4)) * x4(k)+(SS_A_CNT(3,5)) * x5(k) + (SS_A_CNT(3,6)) * x6(k) + SS_B_CNT(3) * phi_soll(k));
-    x4(k + 1) = x4(k) + dt * (SS_A_CNT(4,1) * x1(k)+(SS_A_CNT(4,2)) * x2(k) + (SS_A_CNT(4,3)) * x3(k) + (SS_A_CNT(4,4)) * x4(k)+(SS_A_CNT(4,5)) * x5(k) + (SS_A_CNT(4,6)) * x6(k) + SS_B_CNT(4) * phi_soll(k));
-    x5(k + 1) = x5(k) + dt * (SS_A_CNT(5,1) * x1(k)+(SS_A_CNT(5,2)) * x2(k) + (SS_A_CNT(5,3)) * x3(k) + (SS_A_CNT(5,4)) * x4(k)+(SS_A_CNT(5,5)) * x5(k) + (SS_A_CNT(5,6)) * x6(k) + SS_B_CNT(5) * phi_soll(k));
-    x6(k + 1) = x6(k) + dt * (SS_A_CNT(6,1) * x1(k)+(SS_A_CNT(6,2)) * x2(k) + (SS_A_CNT(6,3)) * x3(k) + (SS_A_CNT(6,4)) * x4(k)+(SS_A_CNT(6,5)) * x5(k) + (SS_A_CNT(6,6)) * x6(k) + SS_B_CNT(6) * phi_soll(k));
-    phi_ist(k) = SS_C_CNT(1) * x1(k) + SS_C_CNT(2) * x2(k) + SS_C_CNT(3) * x3(k) + SS_C_CNT(4) * x4(k) + SS_C_CNT(5) * x5(k) + SS_C_CNT(6) * x6(k);
+    x1(k + 1) = x1(k) + dt * (SS_A(1,1) * x1(k)+(SS_A(1,2)) * x2(k) + (SS_A(1,3)) * x3(k) + (SS_A(1,4)) * x4(k)+(SS_A(1,5)) * x5(k) + (SS_A(1,6)) * x6(k) + SS_B(1) * phi_soll(k));
+    x2(k + 1) = x2(k) + dt * (SS_A(2,1) * x1(k)+(SS_A(2,2)) * x2(k) + (SS_A(2,3)) * x3(k) + (SS_A(2,4)) * x4(k)+(SS_A(2,5)) * x5(k) + (SS_A(2,6)) * x6(k) + SS_B(2) * phi_soll(k));
+    x3(k + 1) = x3(k) + dt * (SS_A(3,1) * x1(k)+(SS_A(3,2)) * x2(k) + (SS_A(3,3)) * x3(k) + (SS_A(3,4)) * x4(k)+(SS_A(3,5)) * x5(k) + (SS_A(3,6)) * x6(k) + SS_B(3) * phi_soll(k));
+    x4(k + 1) = x4(k) + dt * (SS_A(4,1) * x1(k)+(SS_A(4,2)) * x2(k) + (SS_A(4,3)) * x3(k) + (SS_A(4,4)) * x4(k)+(SS_A(4,5)) * x5(k) + (SS_A(4,6)) * x6(k) + SS_B(4) * phi_soll(k));
+    x5(k + 1) = x5(k) + dt * (SS_A(5,1) * x1(k)+(SS_A(5,2)) * x2(k) + (SS_A(5,3)) * x3(k) + (SS_A(5,4)) * x4(k)+(SS_A(5,5)) * x5(k) + (SS_A(5,6)) * x6(k) + SS_B(5) * phi_soll(k));
+    x6(k + 1) = x6(k) + dt * (SS_A(6,1) * x1(k)+(SS_A(6,2)) * x2(k) + (SS_A(6,3)) * x3(k) + (SS_A(6,4)) * x4(k)+(SS_A(6,5)) * x5(k) + (SS_A(6,6)) * x6(k) + SS_B(6) * phi_soll(k));
+    phi_ist(k) = SS_C(1) * x1(k) + SS_C(2) * x2(k) + SS_C(3) * x3(k) + SS_C(4) * x4(k) + SS_C(5) * x5(k) + SS_C(6) * x6(k);
 
     phi_ist(k + 1) = phi_ist(k) + dt * ( -(1/T_t) * phi_ist(k) + (K_p/T_t) * phi_soll(k));
     
-    % Berechnung Position
+    % Berechnung Position Torpedo
     x_t(k + 1) = x_t(k) + v_t * sin(phi_ist(k)) * dt;
     y_t(k + 1) = y_t(k) + v_t * -cos(phi_ist(k)) * dt;
     
 
-    x_z(k + 1) = x_z(k) + v_z * cos(phi_z(k)) * dt;
-    y_z(k + 1) = y_z(k) + v_z * -sin(phi_z(k)) * dt;
-   
     if(sqrt((y_z(k) - y_t(k))^2 + (x_z(k) - x_t(k))^2)<=2000)
-        x_z(k + 1) = x_z(k) + v_z * cos(phi_z(k) + phi_z_zz) * dt;
-        y_z(k + 1) = y_z(k) + v_z * -sin(phi_z(k) + phi_z_zz) * dt;
-       
-        if mod(time,60)==0
+        
+        phi_z(k) = phi_z(k) + phi_z_zz;
+
+        if mod(time,15)==0
             phi_z_zz = -phi_z_zz;
         end
       
     end
+
+    % Berechnung Position Schiff
+    x_z(k + 1) = x_z(k) + v_z * cos(phi_z(k)) * dt;
+    y_z(k + 1) = y_z(k) + v_z * -sin(phi_z(k)) * dt;
+   
+
     
     time = (k - 1) * dt;
     time_vec(k) = (k - 1) * dt;
