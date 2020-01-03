59f1fd17169c262f9afc3c76a69184d349146df7
Jan Schmidt <me@jnschmdt.de>
2020-01-03 20:14:38 +0100

    added HA2, added realtime plot simu

--- torpedo.m
+++ torpedo.m
@@ -17,7 +17,7 @@ timestamp = 0; % [s]
 K_p = 1; % [-] Verstärkung
 T_t = 15; % [s] Zeitkonstante Torpedo
 v_t = 50 * kn2mjs; % [m/s] Geschwindigkeit Torpedo (Knoten -> m/s)
-y_t__start = 4000; % [m] Startentfernung Torpedo <-> Zerstörer
+y_t0 = 4000; % [m] Startentfernung Torpedo <-> Zerstörer
 
 % -- Zerstörer Parameter -- %
 v_z = 30 * kn2mjs; % [m/s] Geschwindigkeit Zerstörer (Knoten -> m/s)
@@ -32,7 +32,7 @@ time_vec = zeros(1, sim_length); % [s] Zeitvektor
 % -- Torpedo Vektoren -- %
 x_t = base_vec; % [m] X-Richtungsvektor Torpedo
 y_t = base_vec; % [m] Y-Richtungsvektor Torpedo
-y_t(1) = y_t__start; % [m] Startentfernung setzen
+y_t(1) = y_t0; % [m] Startentfernung setzen
 
 phi_soll = base_vec; % [rad] Sollwinkel Torpedo
 % phi_t_ist = base_vec; % [rad] Istwinkel Ruder Torpedo (in Rad)
@@ -46,22 +46,18 @@ phi_z = base_vec; % [rad] Winkel Zerstörer
 
 % --Zustandsvektoren -- %
 
-% Zustände der einzelnen Modelle
-RM_x1 = base_vec; % Zustand X1 Rudermaschine
-TRPD_x1 = base_vec; % Zustand X1 Torpedo
-TRPD_x2 = base_vec; % Zustand X2 Torpedo
-MESS_x1 = base_vec; % Zustand X1 Messsystem
-REG_x1 = base_vec; % Zustand X1 Regler
-REG_x2 = base_vec; % Zustand X2 Regler
-
-% Eingangsgröße
-in_REG = base_vec; % Eingangsgröße Regler (phi)
-
-% Ausgangsgrößen
-out_REG = base_vec; % Ausgangsgröße PID (Y_R_PIDT1)
-out_RM = base_vec; % Ausgangsgröße Rudermaschine (y_delta_ist)
-out_TRPD = base_vec; % [rad] Ausgangsgröße Torpedo (phi)
-out_MESS = base_vec; % Ausgangsgröße Messsystem (phi_mess_ist)
+% Vektoren
+u_w = base_vec;
+u_REG = base_vec;
+u_INT = base_vec;
+u_GES = base_vec;
+e_phi = base_vec;
+x1 = base_vec;
+x2 = base_vec;
+x3 = base_vec;
+phi_ist = base_vec;
+x1_MESS = base_vec;
+phi_MESS = base_vec;
 
 %% --- Modellbildung --- %%
 % -- Modell Parameter -- %
@@ -104,68 +100,70 @@ G_REG_N = K_R * [(T_I * (T_DV + T_D)) (T_DV + T_I) 1];
 G_REG_D = [(T_DV * T_I) T_I 0];
 G_REG = tf(G_REG_N, G_REG_D);
 
-% Gesamtsystem
-G_SYS = feedback(series(series(G_REG, G_RM), G_TRPD), G_MESS);
+% Gesamtsystem (Rudermaschine + Torpedo)
+G_SYS = series(G_RM, G_TRPD);
 
 %% --- Zustandsraum --- %%
 
-% Überführung der Rudermaschine in Zustandsmatrizen
-[SS_A_CNT_RM,SS_B_CNT_RM,SS_C_CNT_RM,SS_D_CNT_RM] = tf2ss(G_RM_N, G_RM_D);
+SS_A = [-1 / T_RM 0 0; K_IS / T_Nomoto -1 / T_Nomoto 0; 0 1 0];
+SS_B = [K_RM / T_RM; 0; 0];
+SS_C = [0 0 1];
+SS_D = 0;
 
-% Überführen des Kursverhaltens in Zustandsmatrizen
-[SS_A_CNT_TRPD,SS_B_CNT_TRPD,SS_C_CNT_TRPD,SS_D_CNT_TRPD] = tf2ss(G_TRPD_N, G_TRPD_D);
 
-% Überführen des Messsystems in Zustandsmatrizen
-[SS_A_CNT_MESS,SS_B_CNT_MESS,SS_C_CNT_MESS,SS_D_CNT_MESS] = tf2ss(G_MESS_N, G_MESS_D);
+% Zustandsmatrizen Messsystem
+[SS_A_CNT_MESS, SS_B_CNT_MESS, SS_C_CNT_MESS, SS_D_CNT_MESS] = tf2ss(G_MESS_N, G_MESS_D);
 
-% Überführung des Reglers in Zustandsmatrizen
-[SS_A_CNT_REG,SS_B_CNT_REG,SS_C_CNT_REG,SS_D_CNT_REG] = tf2ss(G_REG_N, G_REG_D);
+% Wunschpole
+sp1 = -0.7;
+sp2 = -2.0 + 1.2i;
+sp3 = -2.0 - 1.2i;
+
+% Wunschpole Vektor
+placed_poles = [sp1 sp2 sp3];
+
+% Reglerparameter
+K_R = place(SS_A, SS_B, placed_poles);
 
 %% --- Berechnung --- %%
 
-% Aufrufen des Contol System Designers
-%sisotool(G_RM * G_PHI, G_PID, G_MESS)
+%sisotool(G_RM * G_TRPD, G_PID, G_MESS)
 
-while( (sqrt((x_t(k) - x_z(k))^2 + (y_t(k) - y_z(k))^2)) >= 5) && ((k * dt) < t_max)
+P_VORFILTER = -(SS_C * ((SS_A - SS_B * K_R)^-1) * SS_B)^-1;
+K_I_VORFILTER = 0.1;
+
+while( (sqrt((x_z(k) - x_t(k))^2 + (y_t(k) - y_z(k))^2)>=5) && ((k * dt)<t_max))
     
     % Berechnung Sollwinkel
     phi_soll(k) = atan2((x_z(k) - x_t(k)), (y_t(k) - y_z(k)));
 
+    u_w(k) = P_VORFILTER * phi_soll(k);
+
     % Berechnung Eingangsgröße Regler
-    in_REG(k) = phi_soll(k) - out_MESS(k);
 
-    % Berechnung Zustände Regler
-    REG_x1(k + 1) = REG_x1(k) + dt * (SS_A_CNT_REG(1,1) * REG_x1(k) + SS_A_CNT_REG(1,2) * REG_x2(k) + SS_B_CNT_REG(1) * in_REG(k));
-    REG_x2(k + 1) = REG_x2(k) + dt * (SS_A_CNT_REG(2,1) * REG_x1(k) + SS_A_CNT_REG(2,2) * REG_x2(k) + SS_B_CNT_REG(2) * in_REG(k));
+    e_phi(k) = phi_soll(k) - phi_MESS(k);
 
-    % Berechnung Ausgang Regler
-    out_REG(k) = SS_C_CNT_REG(1,1) * REG_x1(k) + SS_C_CNT_REG(1,2) * REG_x2(k) + SS_D_CNT_REG * in_REG(k);
+    u_REG(k) = K_R(1,1) * x1(k) + K_R(1,2) * x2(k) + K_R(1,3) * x3(k);
 
-    % Berechnung Zustände Rudermaschine
-    RM_x1(k + 1) = RM_x1(k) + dt * (SS_A_CNT_RM(1,1) * RM_x1(k) + SS_B_CNT_RM(1,1) * out_REG(k));
+    u_INT(k + 1) = u_INT(k) + dt * (K_I_VORFILTER * e_phi(k));
 
-    % Berechnung Ausgangsgröße Rudermaschine
-    out_RM(k) = SS_C_CNT_RM(1,1) * RM_x1(k) + SS_D_CNT_RM * out_REG(k);
+    u_GES(k) = u_w(k) - u_REG(k) + u_INT(k);
 
-    % Berechung Zustände Torpedo
-    TRPD_x1(k + 1) = TRPD_x1(k) + dt * (SS_A_CNT_TRPD(1,1) * TRPD_x1(k) + SS_A_CNT_TRPD(1,2) * TRPD_x2(k) + SS_B_CNT_TRPD(1) * out_RM(k));
-    TRPD_x2(k + 1) = TRPD_x2(k) + dt * (SS_A_CNT_TRPD(2,1) * TRPD_x1(k) + SS_A_CNT_TRPD(2,2) * TRPD_x2(k) + SS_B_CNT_TRPD(2) * out_RM(k));
+    x1(k + 1) = x1(k) + dt * (SS_A(1,1) * x1(k) + SS_A(1,2) * x2(k) + SS_A(1,3) * x3(k) + SS_B(1) * u_GES(k)); % Aktuatorgleichung
+    x2(k + 1) = x1(k) + dt * (SS_A(2,1) * x1(k) + SS_A(2,2) * x2(k) + SS_A(2,3) * x3(k) + SS_B(2) * u_GES(k)); % Bewegnungsgleichung
+    x3(k + 1) = x1(k) + dt * (SS_A(3,1) * x1(k) + SS_A(3,2) * x2(k) + SS_A(3,3) * x3(k) + SS_B(3) * u_GES(k)); % Ausgangsgleichung
+    
+    phi_ist(k) = SS_C(1,1) * x1(k) + SS_C(1,2) * x2(k) + SS_C(1,3) * x3(k) + SS_D * u_GES(k);
 
-    % Berechnung Ausgangsgrößen Torpedo
-    out_TRPD(k + 1) = SS_C_CNT_TRPD(1,1) * TRPD_x1(k) + SS_C_CNT_TRPD(1,2) * TRPD_x2(k) + SS_D_CNT_TRPD * out_RM(k);
+    x1_MESS(k + 1) = x1_MESS(k) + dt * (SS_A_CNT_MESS(1,1) * x1_MESS(k) + SS_B_CNT_MESS(1) * phi_ist(k));
 
-    % Berechnung Zustände Messsystem
-    MESS_x1(k + 1) = MESS_x1(k) + dt * (SS_A_CNT_MESS(1,1) * MESS_x1(k) + SS_B_CNT_MESS(1) * out_TRPD(k));
+    phi_MESS(k) = SS_C_CNT_MESS(1) * x1_MESS(k) + SS_D_CNT_MESS * phi_ist(k);
 
-    % Berechnung Ausgangsgrößen Messsystem
-    out_MESS(k) = SS_C_CNT_MESS(1,1) * MESS_x1(k) + SS_D_CNT_MESS * out_TRPD(k);
+    phi_MESS(k + 1) = phi_MESS(k);
 
-    % Trick 17 ;)
-    out_MESS(k+1) = out_MESS(k);
-
-    % Berechnung Position Torpedo (x und y Richtung)
-    x_t(k + 1) = x_t(k) + dt * v_t * sin(out_TRPD(k)) ; 
-    y_t(k + 1) = y_t(k) + dt * v_t * -cos(out_TRPD(k));
+    % Berechnung Position Torpedo
+    x_t(k + 1) = x_t(k) + v_t * sin(phi_ist(k)) * dt;
+    y_t(k + 1) = y_t(k) + v_t * -cos(phi_ist(k)) * dt;
     
     % Zick Zack Kurs Zerstörer -> wenn Torpedo näher als 2000m
     if(sqrt((y_z(k) - y_t(k))^2 + (x_z(k) - x_t(k))^2)<=2000)
@@ -182,7 +180,6 @@ while( (sqrt((x_t(k) - x_z(k))^2 + (y_t(k) - y_z(k))^2)) >= 5) && ((k * dt) < t_
     x_z(k + 1) = x_z(k) + dt * v_z * cos(phi_z(k));
     y_z(k + 1) = y_z(k) + dt * v_z * -sin(phi_z(k));
    
-   
     % Zeitstempel speichern
     timestamp = (k - 1) * dt;
     time_vec(k) = timestamp;
@@ -191,12 +188,58 @@ while( (sqrt((x_t(k) - x_z(k))^2 + (y_t(k) - y_z(k))^2)) >= 5) && ((k * dt) < t_
     k = k + 1;
 end
 
-%% Darstellung und Anzeige
-figure('Name', 'Seekarte')
-plot (x_t(1:k),y_t(1:k),'r')
+%% Darstellung Laufbahnen
+figure('Name', 'Seekarte', 'Position', [5 5 800 1000])
+plot (x_t(1:k),y_t(1:k),'rs', 'MarkerSize', 3)
 hold on
-plot (x_z(1:k),y_z(1:k),'b')
+plot (x_z(1:k),y_z(1:k),'bd', 'MarkerSize', 3)
 legend('Laufbahn Torpedo','Laufbahn Zerstörer')
 hold off
 grid on
 axis equal
+axis([-2000 4000 -3000 4000])
+
+
+%% Darstellung Laufbahn in "Echtzeit"
+plotRealTime(sim_length, x_t,y_t,x_z,y_z)
+
+% functions declaration
+function plotRealTime(sim_length, x_t, y_t, x_z, y_z)
+    xtData = zeros(1, sim_length);
+    ytData = zeros(1, sim_length);
+    xzData = zeros(1, sim_length);
+    yzData = zeros(1, sim_length);
+
+    xtData(1) = x_t(1);
+    ytData(1) = y_t(1);
+    xzData(1) = x_z(1);
+    yzData(1) = y_z(1);
+
+    figure('Name', 'Seekarte Echtzeit', 'Position', [1000 5 800 1000])
+    hold on
+    tplot = plot (xtData(1), ytData(1), 'rs', 'MarkerSize', 3);
+    zplot = plot (xzData(1), yzData(1), 'bd', 'MarkerSize', 3);
+    legend('Laufbahn Torpedo','Laufbahn Zerstörer')
+    grid on
+    axis equal
+    axis([-2000 4000 -3000 4000])
+    i = 1;
+    j = 2;
+    while (sqrt((x_z(i) - x_t(i))^2 + (y_t(i) - y_z(i))^2)>=5)
+        if mod(i, 5000) == 0
+            xtData(j) = x_t(i);
+            ytData(j) = y_t(i);
+            xzData(j) = x_z(i);
+            yzData(j) = y_z(i);
+
+            set(tplot, 'XData', xtData(1:i), 'YData', ytData(1:i))
+            set(zplot, 'XData', xzData(1:i), 'YData', yzData(1:i))
+            j = j + 1;
+        end
+        i = i + 1;
+        pause(0.00001)
+    end
+
+    hold off
+end
+
